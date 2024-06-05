9f3a1a1ee3d2991f12f7105212f03e3fcac0a890
rudyhuacasi <alexisitaliano16@gmail.com>
2024-06-05 22:13:58 +0200

    {UPD} addec header and footer (boolando)

--- vite-project/src/components/footerComponent.vue
+++ vite-project/src/components/footerComponent.vue
@@ -8,4 +8,52 @@ export default {
     }
 }
 </script>
-<template></template>
+<template>
+    <footer>
+        <div class="container">
+            <div class="flex">
+                <div class="medium">
+                    <strong>Booleando s.r.l.</strong><br>
+                    <p class="top">Informazioni legali Inormativa sulla privacy Diritto di recesso</p>
+                    
+                </div>
+                <div class=" right-1 medium">
+                    <p>Trovaci anche su</p>
+                        <i class="fa-brands fa-square-twitter"></i>
+                        <i class="fa-brands fa-square-facebook"></i>
+                        <i class="fa-brands fa-square-instagram"></i>
+                        <i class="fa-brands fa-square-pinterest"></i>
+                        <i class="fa-brands fa-square-youtube"></i>
+                    
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<style lang="scss">
+%flex{
+    display: flex;
+    align-items: center;
+}
+footer{
+    @extend %flex;
+    background-color: black;
+    height: 75px;
+    .flex{
+        @extend %flex;
+    }
+    .medium{
+        color: white;
+        width: 50%;
+    }
+    .right-1{
+        text-align: right;
+    }
+    p{
+        margin-bottom: 5px;
+    }
+    .top{
+        margin-top: 5px;
+    }
+}
+</style>
--- vite-project/src/components/headerComponent.vue
+++ vite-project/src/components/headerComponent.vue
@@ -1,13 +1,69 @@
 <script>
-  export default {
-    name: app,
-    data() {
-      return {
-        title: "La mia prima app con Vite!"
-      };
-    }
-  }
+  
 </script>
 <template>
-
+  <!--header-->
+      <header>
+          <div class="container">
+              <div class="d-flex">
+                  <div class="small">
+                      <p> Donna  Uomo  Bambini </p>
+                  </div>
+                  <div class="large">
+                      <img class="boolean" src="./img/boolean-logo.png" alt="">
+                  </div>
+                  <div class="small icona">
+                      <i class="fa-regular fa-user"></i>
+                      <i class="fa-regular fa-heart"></i>
+                      <i class="fa-solid fa-bag-shopping"></i>
+                  </div>
+              </div>
+          </div>
+      </header>
 </template>
+
+<style lang="scss">
+
+%message-shared {
+  width: 20%;
+  color: white;
+}
+
+header{
+    background-color: rgb(253, 114, 0);
+    height: 70px;
+    display: flex;
+    align-items: center;
+    position: fixed;
+    width: 100%;
+    z-index: 20;
+    border-bottom: 1px solid silver;
+
+  .d-flex{
+    display: flex;
+    align-items: center;
+  }
+
+  .small{
+    @extend %message-shared;
+  }
+
+  .large{
+    width: 60%;
+    text-align: center;
+  }
+
+  .boolean{
+    width: 180px;
+  }
+
+  .icona{
+    @extend %message-shared;
+    text-align: right;
+  }
+  
+}
+i{
+    padding: 5px;
+  }
+</style>
